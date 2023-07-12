f99fef200b797c4ad04234d743f1c358da4d8667
Artur Sultanov <sultanov.personal@gmail.com>
2023-07-12 10:28:33 +0200

    first commit

---
+++ python-proj-00/name.py
@@ -0,0 +1,15 @@
+# string operations
+
+name = "ada lovelace"
+name = name.title()
+print(name)
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "loverlace"
+full_name = first_name + " " + last_name
+print(full_name)
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
