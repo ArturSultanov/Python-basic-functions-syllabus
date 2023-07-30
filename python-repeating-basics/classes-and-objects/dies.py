7029ca611064891434fff1c229ee734523299c72
Artur Sultanov <sultanov.personal@gmail.com>
2023-07-30 20:50:06 +0200

    update

--- python-repeating-basics/classes-and-objects/dies.py
+++ python-repeating-basics/classes-and-objects/dies.py
@@ -1,13 +1,20 @@
 from random import randint
 
-class Die():
+class RollingDie(): #CamelCase style
+    """Class of the rolling die."""
     def __init__(self, sides=6):
+        """
+        Initializes attributes specific to the die.
+        """
         self.sides = sides
 
     def roll_die(self):
+        """
+        Outputs information about the result of the die roll.
+        """
         x = randint(1, self.sides)
         print(f"Score of the {self.sides}-side die roll is {x}!")
         
-die_six = Die()
+die_six = RollingDie()
 for i in range(10):
     die_six.roll_die()
