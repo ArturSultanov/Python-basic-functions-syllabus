e0f3f72e378124d0bf16631ffe12e4ae4c6b21ab
Artur Sultanov <sultanov.personal@gmail.com>
2023-08-02 22:33:48 +0200

    update

--- python-repeating-basics/json/remember_me.py
+++ python-repeating-basics/json/remember_me.py
@@ -1,14 +1,26 @@
 import json
 
-filename = 'username.json'
+def get_stored_username():
+    """Get saved username, if it is exist."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
 
-try:
-    with open(filename) as f_obj:
-        username = json.load(f_obj)
-except FileNotFoundError:
-    username = input("What is your name? ")
-    with open(filename, 'w') as file_obj:
-        json.dump(username, file_obj)
-        print("We'll remember you when you come back, " + username +"!")
-else:
-    print("Wellcom back, " + username + "!")
+def greet_user():
+    """Greeting of user using them name.""" 
+    username = get_stored_username()
+    if username:
+        print("Welcome back, " + username + "!")
+    else:
+        username = input("What is your name? ")
+        filename = 'username.json'
+        with open(filename, 'w') as f_obj:
+            json.dump(username, f_obj)
+            print("We'll remember you when you come back, " + username + "!")
+
+greet_user()
